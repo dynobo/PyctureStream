9f53783bb2d4fdb4b65fd5c93217230ce0e9cb39
dynobo <holger.buech@mailbox.org>
2018-02-04 16:03:54 +0100

    Remove deprc. line

--- setup_cloudera.sh
+++ setup_cloudera.sh
@@ -3,8 +3,8 @@
 HOME = "/home/cloudera"
 
 # Install Anaconda with Jupyter
-if [ ! -d "$HOME/anaconda3" ]; then
-    wget https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh && chmod +x Anaconda3-5.0.1-Linux-x86_64.sh && ./Anaconda3-5.0.1-Linux-x86_64.sh
+if [ ! -d "$HOME/anaconda2" ]; then
+    wget https://repo.continuum.io/archive/Anaconda2-5.0.1-Linux-x86_64.sh && chmod +x Anaconda3-5.0.1-Linux-x86_64.sh && ./Anaconda3-5.0.1-Linux-x86_64.sh
 else
     echo "Anaconda3 was already installed."
 fi
@@ -17,5 +17,3 @@ mkdir "$HOME/notebooks"
 # Enable Cron
 CMD='/sbin/runuser cloudera -s /bin/bash -c "/home/cloudera/start_jupyter.sh &"'
 grep -q -F "$CMD" /etc/rc.d/rc.local || echo "$CMD" | sudo tee -a /etc/rc.d/rc.local
-
-sudo update-rc.d jupyter_service defaults
