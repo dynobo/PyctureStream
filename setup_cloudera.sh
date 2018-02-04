967b1c4900786ef288978ff899fc7d8642887854
dynobo <holger.buech@mailbox.org>
2018-02-04 16:07:58 +0100

    Fix some paths

--- setup_cloudera.sh
+++ setup_cloudera.sh
@@ -4,7 +4,7 @@ HOME = "/home/cloudera"
 
 # Install Anaconda with Jupyter
 if [ ! -d "$HOME/anaconda2" ]; then
-    wget https://repo.continuum.io/archive/Anaconda2-5.0.1-Linux-x86_64.sh && chmod +x Anaconda3-5.0.1-Linux-x86_64.sh && ./Anaconda3-5.0.1-Linux-x86_64.sh
+    wget https://repo.continuum.io/archive/Anaconda2-5.0.1-Linux-x86_64.sh && chmod +x Anaconda2-5.0.1-Linux-x86_64.sh && ./Anaconda3-2.0.1-Linux-x86_64.sh
 else
     echo "Anaconda3 was already installed."
 fi
@@ -12,7 +12,9 @@ fi
 # Download Start Script for Jupyter with pySpark
 rm -f "$HOME/start_jupyter.sh"
 wget https://raw.githubusercontent.com/dynobo/PyctureStream/master/start_jupyter.sh && chmod +x ./start_jupyter.sh
-mkdir "$HOME/notebooks"
+if [ ! -d "$HOME/notebooks" ]; then
+    mkdir "$HOME/notebooks"
+fi
 
 # Enable Cron
 CMD='/sbin/runuser cloudera -s /bin/bash -c "/home/cloudera/start_jupyter.sh &"'
