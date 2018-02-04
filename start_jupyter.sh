8fbdf5ce58e604daf5ec1f1b7516204e8b464387
dynobo <holger.buech@mailbox.org>
2018-02-04 16:06:49 +0100

    Switched to python2

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export PYSPARK_DRIVER_PYTHON="/home/cloudera/anaconda3/bin/jupyter"
+export PYSPARK_DRIVER_PYTHON="/home/cloudera/anaconda2/bin/jupyter"
 export PYSPARK_DRIVER_PYTHON_OPTS="notebook --NotebookApp.token='' --port=8889 --ip=0.0.0.0 --notebook-dir=/home/cloudera/notebooks"
 pyspark
