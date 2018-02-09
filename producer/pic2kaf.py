590e5c581101ad76d32d3f6c8095cf6dc193feee
dynobo <holger.buech@mailbox.org>
2018-02-09 19:34:26 +0100

    Make json payload work

--- producer/pic2kaf.py
+++ producer/pic2kaf.py
@@ -1,55 +1,92 @@
-#from __future__ import print_function
+"""Kafka-Producer that's sending video frames from a device
+or a Video File to an Kafka Endpoint."""
+
 from kafka import KafkaProducer
 import cv2
+import base64
 import json
 import time
 import datetime as dt
 import logging
 logging.basicConfig(format='%(levelname)s - %(asctime)s: %(message)s',
-                    datefmt='%m/%d/%Y %H:%M:%S',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 class webcam_producer():
-    """Stream WebCam Images to Kafka Endpoint."""
+    """Stream WebCam Images to Kafka Endpoint.
 
-    def __init__(self, source: int, interval: int = 5, server: str = '127.0.0.1:9092', *args, **kwargs):
+    Keyword arguments:
+    source -- Index of Video Device or Filename of Video-File
+    interval -- Interval for capturing images in seconds (default 5)
+    server -- Host + Port of Kafka Endpoint (default '127.0.0.1:9092')
+    """
 
-        self.video_source = source if source else 'demo.mp4' # Index of Webcam or Demo Video
+    def __init__(self,
+                 interval: int = 5,
+                 source=0,
+                 camera_id: str = 'camera_generic',
+                 topic: str = 'wordcounttopic',
+                 server: str = '127.0.0.1:9092'):
+
+        logger.info(f'Initialized camera "{camera_id}" with source {source}.')
+        logger.info(f'Send to "{topic}" on "{server}" every {interval} sec.')
+
+        # Class Variables
         self.interval = interval  # Interval for Photos in Seconds
+        self.video_source = source
+        self.camera_id = camera_id
         self.server = server  # Host + Port of Kafka Endpoint
+        self.topic = topic
+        self.img_file = './frame.jpg'
 
-        # TODO: Images are somehow queued up by cv2!
-        self.vidcap = cv2.VideoCapture(0)
-        logger.info('Initialized.')
-        self.start_stream()
+        # Connection to Kafka Enpoint
+        self.producer = KafkaProducer(bootstrap_servers=self.server,
+                                      value_serializer=lambda m: json.dumps(m).encode('utf8'))
+        self.producer.send(self.topic, b'data')
 
-    def take_picture(self):
+        # Start Streaming...
+        self.stream_video()
 
-        success, image = self.vidcap.read()
-        if success is True:
-            return image
-        else:
-            logger.warn('Video capturing unsuccessful.')
-            return False
-
-    def start_stream(self):
-        logger.info('Starting Streaming.')
+    def stream_video(self):
+        """Start streaming video frames to Kafka forever."""
+        logger.info(f'Start capturing frames every {self.interval} sec.')
         while True:
-            pic = self.take_picture()
+            vidcap = cv2.VideoCapture(self.video_source)
+            success, image = vidcap.read()
             timestamp = dt.datetime.utcnow().isoformat()
-            if pic is not False:
-                cv2.imwrite('frame.jpg', pic) # Save frame as JPEG file, for debug
-                logger.info(f'Toke Picture at {timestamp}.')
+            vidcap.release()
+            if success is True:
+                # Base64 encode image for transfer in json
+                png = cv2.imencode('.png', image)[1]
+                png_as_text = base64.b64encode(png).decode('utf-8')
+                # Build object and send to Kafka
+                result = {
+                    'image': png_as_text,
+                    'timestamp': timestamp,
+                    'camera_id': self.camera_id
+                }
+                self.send_to_kafka(result)
+                self.save_image(image)
+            else:
+                logger.error(f'Could not read image from source {self.video_source}!')
+            # Sleep interval, before next capture
             time.sleep(self.interval)
 
-    def send_to_kafka(self):
-        producer = KafkaProducer(bootstrap_servers=self.server,
-                            value_serializer=lambda m: json.dumps(m).encode('utf8'))
-        for _ in range(25):
-            print('Sending...')
-            producer.send('wordcounttopic', {'foo':'b♥ar'})
+    def send_to_kafka(self, data):
+        """Send JSON payload to topic in Kafka."""
+        self.producer.send(self.topic, data)
+        logger.info('Sent image to Kafka endpoint.')
+
+    def save_image(self, image):
+        """Save frame as JPEG file, for debugging purpose only."""
+        cv2.imwrite(self.img_file, image)
+        logger.info(f'Saved image file to {self.img_file}.')
 
 if __name__ == '__main__':
-    webcam_producer(interval=5, source=1, server='127.0.0.1:9092')
+    # Set source='demo.mp4' for streaming video file
+    webcam_producer(interval=5,
+                    source=0,
+                    camera_id='holger',
+                    server='127.0.0.1:9092',
+                    topic='wordcounttopic')
--- producer/test_webcam.py
+++ producer/test_webcam.py
@@ -1,9 +1,12 @@
+"""Test if opencv works on the computer and can get video device."""
+
 import sys
 import cv2
 
 
 def main(argv):
-    #capture from camera at location 0
+    """Output various parameters of video device."""
+    # capture from camera at location 0
     cap = cv2.VideoCapture(0)
     # Change the camera setting using the set() function
     # cap.set(cv2.CAP_PROP_EXPOSURE, -6.0)
@@ -35,20 +38,14 @@ def main(argv):
     print("Hue: ", hue)
     print("Gain: ", gain)
     print("Exposure: ", exposure)
-    # while True:
-        # ret, img = cap.read()
-        # cv2.imshow("input", img)
-        #
-        # key = cv2.waitKey(10)
-        # if key == 27:
-        #     break
 
     cv2.destroyAllWindows()
     cv2.VideoCapture(0).release()
 
 
 #   0  CV_CAP_PROP_POS_MSEC Current position of the video file in milliseconds.
-#   1  CV_CAP_PROP_POS_FRAMES 0-based index of the frame to be decoded/captured next.
+#   1  CV_CAP_PROP_POS_FRAMES 0-based index of the frame to be decoded/captured
+#      next.
 #   2  CV_CAP_PROP_POS_AVI_RATIO Relative position of the video file
 #   3  CV_CAP_PROP_FRAME_WIDTH Width of the frames in the video stream.
 #   4  CV_CAP_PROP_FRAME_HEIGHT Height of the frames in the video stream.
@@ -56,16 +53,19 @@ def main(argv):
 #   6  CV_CAP_PROP_FOURCC 4-character code of codec.
 #   7  CV_CAP_PROP_FRAME_COUNT Number of frames in the video file.
 #   8  CV_CAP_PROP_FORMAT Format of the Mat objects returned by retrieve() .
-#   9 CV_CAP_PROP_MODE Backend-specific value indicating the current capture mode.
+#   9 CV_CAP_PROP_MODE Backend-specific value indicating the current capture
+#     mode.
 #   10 CV_CAP_PROP_BRIGHTNESS Brightness of the image (only for cameras).
 #   11 CV_CAP_PROP_CONTRAST Contrast of the image (only for cameras).
 #   12 CV_CAP_PROP_SATURATION Saturation of the image (only for cameras).
 #   13 CV_CAP_PROP_HUE Hue of the image (only for cameras).
 #   14 CV_CAP_PROP_GAIN Gain of the image (only for cameras).
 #   15 CV_CAP_PROP_EXPOSURE Exposure (only for cameras).
-#   16 CV_CAP_PROP_CONVERT_RGB Boolean flags indicating whether images should be converted to RGB.
+#   16 CV_CAP_PROP_CONVERT_RGB Boolean flags indicating whether images should
+#      be converted to RGB.
 #   17 CV_CAP_PROP_WHITE_BALANCE Currently unsupported
-#   18 CV_CAP_PROP_RECTIFICATION Rectification flag for stereo cameras (note: only supported by DC1394 v 2.x backend currently)
+#   18 CV_CAP_PROP_RECTIFICATION Rectification flag for stereo cameras (note
+#      only supported by DC1394 v 2.x backend currently)
 
 if __name__ == '__main__':
     main(sys.argv)
