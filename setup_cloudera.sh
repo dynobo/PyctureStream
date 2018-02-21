2dbf915cd620c88762e3023bcabc0a1f57f5ab4d
dynobo <holger.buech@mailbox.org>
2018-02-21 19:17:01 +0100

    Add python packages to setup script

--- setup_cloudera.sh
+++ setup_cloudera.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 HOME="/home/cloudera"
 
 # Install Kafka
@@ -11,15 +10,17 @@ sudo yum install -y kafka-server
 sudo sed '/^listeners=/ d' /etc/kafka/conf.dist/server.properties
 sudo sed '/^advertised.listeners=/ d' /etc/kafka/conf.dist/server.properties
 echo "# Settings for PyctureStream Project" | sudo tee -a /etc/kafka/conf.dist/server.properties
-echo "listeners=PLAINTEXT://0.0.0.0:9092" | sudo tee -a /etc/kafka/conf.dist/server.properties
-echo "advertised.listeners=PLAINTEXT://0.0.0.0:9092" | sudo tee -a /etc/kafka/conf.dist/server.properties
+echo "listeners=PLAINTEXT://127.0.0.1:9092" | sudo tee -a /etc/kafka/conf.dist/server.properties
+echo "advertised.listeners=PLAINTEXT://127.0.0.1:9092" | sudo tee -a /etc/kafka/conf.dist/server.properties
 
 # Start Kafka Server
 sudo service kafka-server start
 
-# Install Anaconda with Jupyter
+# Install Anaconda with Jupyter + additional packages
 if [ ! -d "$HOME/anaconda2" ]; then
     wget https://repo.continuum.io/archive/Anaconda2-5.0.1-Linux-x86_64.sh && chmod +x Anaconda2-5.0.1-Linux-x86_64.sh && ./Anaconda2-5.0.1-Linux-x86_64.sh
+    conda install -c conda-forge jupyterlab opencv
+    conda install nose keras pillow h5py py4j
 else
     echo "Anaconda3 was already installed."
 fi
